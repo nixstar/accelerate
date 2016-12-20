49e1e2410bb8c84408e27d2589ecdc36af605b47
Nixst@r <oneikap@gmail.com>
2016-12-20 19:35:18 +1000

    add custom posts

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -24,3 +24,18 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 1.0
  */
+ 
+ function create_custom_post_types() {
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
