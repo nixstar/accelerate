1adb24bc5c7c4d4e96e9f851e45ddb57a6a157b7
Nixst@r <oneikap@gmail.com>
2017-01-03 21:07:52 +1000

    contact form and success page added

---
+++ wp-content/themes/accelerate-theme-child/page-success.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying the success page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="success-page" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<h2 id="centred">Thank you for your message!</h2>
+				<p class="centred">We will get back to you ASAP</p>
+
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
