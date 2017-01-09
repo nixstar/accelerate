8dbaf9aefbda9191aba6a6c8ac4e719159e1e015
Nixst@r <oneikap@gmail.com>
2017-01-09 10:58:11 +1000

    about page html added

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -24,5 +24,49 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+<section class="about-page-content">
+
+	<div class="our-services">
+		<h4>Our Services</h4>
+		<p>We take pride in our clients and the content we create for them.<br>
+			Here's a brief overview of our offered services.</p>
+	</div>
+
+	<div class="strategies">
+		<img src="content-strategy.png" alt="arrow icon">
+		<h2>Content Strategy</h2>
+		<p>Dolore consequat esse anim reprehenderit est aliqua anim cillum sit dolore. Ullamco mollit velit eu ad pariatur excepteur Lorem cupidatat duis velit veniam. Eiusmod proident reprehenderit velit id non voluptate sint in adipisicing dolore sunt cillum id.</p>
+	</div>
+
+	<div class="strategies-alt">
+		<img src="influencer-mapping.png" alt="atom icon">
+		<h2>Influencer Mapping</h2>
+		<p>Dolore consequat esse anim reprehenderit est aliqua anim cillum sit dolore. Ullamco mollit velit eu ad pariatur excepteur Lorem cupidatat duis velit veniam. Eiusmod proident reprehenderit velit id non voluptate sint in adipisicing dolore sunt cillum id.</p>
+	</div>
+
+	<div class="strategies">
+		<img src="social-media-strategy.png" alt="thumbs-up icon">
+		<h2>Social Media Strategy</h2>
+		<p>Dolore consequat esse anim reprehenderit est aliqua anim cillum sit dolore. Ullamco mollit velit eu ad pariatur excepteur Lorem cupidatat duis velit veniam. Eiusmod proident reprehenderit velit id non voluptate sint in adipisicing dolore sunt cillum id.</p>
+	</div>
+
+	<div class="strategies-alt">
+		<img src="design-development.png" alt="wand icon">
+		<h2>Design &amp Development</h2>
+		<p>Dolore consequat esse anim reprehenderit est aliqua anim cillum sit dolore. Ullamco mollit velit eu ad pariatur excepteur Lorem cupidatat duis velit veniam. Eiusmod proident reprehenderit velit id non voluptate sint in adipisicing dolore sunt cillum id.</p>
+	</div>
+
+</section>
+
+<section class="work-with-us">
+	<div class="interested">
+	<h4>Interested in working with us?</h4>
+	</div>
+
+	<div class="contact-us-btn">
+		<a class="button" href="<?php echo home_url(); ?>/contact-us">Contact Us</a>
+	</div>
+
+</section>
 
 <?php get_footer(); ?>
